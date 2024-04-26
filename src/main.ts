46b90f95ee5b963cf698b532f806e5168245364a
scarlet <scarryaa@gmail.com>
2024-04-26 14:30:55 -0400

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,8 @@
-import { invoke } from "@tauri-apps/api/tauri";
+import "./styles.css";
+import App from "./App.svelte";
 
-let greetInputEl: HTMLInputElement | null;
-let greetMsgEl: HTMLElement | null;
-
-async function greet() {
-  if (greetMsgEl && greetInputEl) {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    greetMsgEl.textContent = await invoke("greet", {
-      name: greetInputEl.value,
-    });
-  }
-}
-
-window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form")?.addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
-  });
+const app = new App({
+  target: document.getElementById("app"),
 });
+
+export default app;
